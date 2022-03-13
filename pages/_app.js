38dc2712635a97e1a1a95fb3af28525e73f7e2ad
abbas ebadian <abbasebadiann@gmail.com>
2022-03-13 12:08:59 +0330

    init

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/contact-us/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../../styles/ContactUs.module.css'
+function ContactUs() {
+  return (
+    <div className={styles.contact_title + " text-danger"} >ContactUs</div>
+  )
+}
+
+export default ContactUs
